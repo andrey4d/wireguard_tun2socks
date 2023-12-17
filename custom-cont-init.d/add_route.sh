b4afd08f9b1a2fd81edeb67a8bebb59e0dc38133
Andrey Danilov <andrey4d.dev@gmail.com>
2023-12-17 12:51:14 +0700

    FIX add_route

--- custom-cont-init.d/add_route.sh
+++ custom-cont-init.d/add_route.sh
@@ -7,5 +7,6 @@ ROUTE_TABLE=${ROUTE_TABLE:-"120"}
 TUNNEL_IP=$(ping -c 1 ${TUNNEL_GW}  | awk 'FNR==2 {print $5}' | awk 'match($0, /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/) {print substr($0, RSTART, RLENGTH)}')
 echo "TUNNEL_IP: ${TUNNEL_IP}"
 
-ip route add default via ${TUNNEL_IP} table ${ROUTE_TABLE}
-ip rule add from ${INTERNAL_SUBNET}/24 table ${ROUTE_TABLE}
+ip route add default via ${TUNNEL_IP} table "${ROUTE_TABLE}"
+ip rule add from "${INTERNAL_SUBNET}/24" table "${ROUTE_TABLE}"
+ip rule add from "${INTERNAL_SUBNET%.0}.1" prohibit
